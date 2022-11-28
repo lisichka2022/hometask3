046a8da1c32b3e323995d65d8b162f757145a34a
lisichka2022 <99811204+lisichka2022@users.noreply.github.com>
2022-11-28 13:48:38 +0200

    Add files via upload

---
+++ calculate.js
@@ -0,0 +1,24 @@
+'use strict'
+let numb1=+prompt('First number');
+let numb2=+prompt('Second number');
+ function  add() {
+    let result=0;
+    result=numb1+numb2;
+   return result; }
+    console.log(`Результат: ${numb1} + ${numb2} = ${add}`);
+    function  sub() {
+        let result=0;
+        result=numb1-numb2;
+       return result; }
+        console.log(`Результат: ${numb1} - ${numb2} = ${sub}`);
+        function  mulyiply() {
+            let result=0;
+            result=numb1*numb2;
+           return result; }
+            console.log(`Результат: ${numb1} * ${numb2} = ${multiply}`);
+    
+            function  div() {
+                let result=0;
+                result=numb1/numb2;
+               return result; }
+                console.log(`Результат: ${numb1} / ${numb2} = ${div}`);
